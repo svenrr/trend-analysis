4dadc524d6d2a3f968b4ff7db37721a54618fb07
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-12 17:15:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import streamlit as st
-from news_scraping import get_urls_from_feed
+import pandas as pd 
+from pytrends.request import TrendReq
+import time
+import datetime
 
-st.write("# BBC News Scraper")
+#from news_scraping import get_urls_from_feed
+#
+#st.write("# BBC News Scraper")
+#
+#if st.button("Fetch..."): 
+#  get_urls_from_feed()
 
-if st.button("Fetch..."): 
-  get_urls_from_feed()
+
+pytrends = TrendReq(hl='en-US', tz=360)
+keywords = ["Joe Biden", "Donald Trump"]
+pytrends.build_payload(keywords, timeframe="today 5-y", geo="US")
+df = pytrends.interest_over_time()
+
+st.write("# Keyword Search")
+
+st.dataframe(df)
+
+df1 = pytrend.trending_searches(pn='germany')
+
+st.write("# Trending Search")
+st.dataframe(df1)
