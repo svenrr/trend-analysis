a5cf046d3d48c509487bebe286164bd62dcb438c
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-16 15:47:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ twitter_trends = pd.read_csv("https://docs.google.com/spreadsheets/d/1ZQmt6uL-MY
 st.write("Last update: {}".format(twitter_trends["date_time"][0]))
 st.dataframe(twitter_trends[["trends", "tweet_volume"]])      
 
-c = alt.Chart(twitter_trends).mark_bar().encode(x=twitter_trends["tweet_volume"], y=twitter_trends["trends"])
+c = alt.Chart(twitter_trends).mark_bar().encode(x="twitter_trends["tweet_volume"]:Q", y="twitter_trends["trends"]:O")
 st.altair_chart(c)
 
 #################
