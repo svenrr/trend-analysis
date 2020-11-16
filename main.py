78e2d5e9f6d110e6464169273ad5a6bc2d682202
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-16 19:19:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -80,10 +80,10 @@ st.dataframe(rq_data)
 
 st.markdown("**Interest over time (US)**")
 sb_tf = st.selectbox("Select a timeframe", ["today 5-y"] , key="sb_tf")
-iot_kws = st.text_input("Enter keywords and use comma as delimiter","['python', 'java', 'html', 'javascript', 'sql']", key="giot")
-#keywords = iot_kws
+iot_kws = st.text_input("Enter keywords and use comma as delimiter","python, java, html, javascript, sql]", key="giot")
+keywords = iot_kws.split("delimiter")
 st.write(type(iot_kws))
-keywords = ['python', 'java', 'html', 'javascript', 'sql']
+#keywords = ['python', 'java', 'html', 'javascript', 'sql']
 interest_over_time = pytrend.build_payload(keywords, timeframe=sb_tf, geo="US")
 iot = pd.DataFrame(pytrend.interest_over_time())
 iot.drop("isPartial",axis=1, inplace=True)
